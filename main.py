2992493cdf8ad1fb585f2701e4f1c64ff7482e3c
AYehia0 <ahmedyehia611@gmail.com>
2021-02-23 21:06:41 +0200

    drew all needed shapes

--- main.py
+++ main.py
@@ -5,10 +5,20 @@ import sys
 # Every pygame must have init()
 pygame.init()
 
+
+# Screen dims
 screen_width, screen_height = 900, 600
 mid_pos_x, mid_pos_y = screen_width //2 , screen_height//2     # Not in the middle but close ,as shapes are rects 
+
+# Colours 
+bg_col = pygame.Color('grey12')
+white_col = (255,255,255)
+
+# Clock
 clk = pygame.time.Clock()
 fps = 60 
+
+# Screen
 screen = pygame.display.set_mode((screen_width, screen_height))
 
 # Setting the caption aka title 
@@ -21,8 +31,8 @@ margin = 15
 
 # Game char rects
 ball = pygame.Rect(mid_pos_x-ball_width//2, mid_pos_y-ball_height//2, ball_width, ball_height)
-player_right = pygame.Rect(screen_width-margin , screen_height-players_height//2 ,players_width, players_height)
-player_left = pygame.Rect(margin , screen_height-players_height//2 ,players_width, players_height)
+player_right = pygame.Rect(screen_width-margin , screen_height//2 - players_height//2 ,players_width, players_height)
+player_left = pygame.Rect(margin//2 ,screen_height//2 - players_height//2 ,players_width, players_height)
 
 # Main loop
 while True:
@@ -33,7 +43,15 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+    
 
+    #drawing 
+    screen.fill(bg_col)
+    pygame.draw.rect(screen, white_col, player_left) # surface , color , object to draw
+    pygame.draw.rect(screen, white_col, player_right) # surface , color , object to draw
+    pygame.draw.ellipse(screen, white_col, ball)
+    
+    
     # updating vars
     pygame.display.flip()
     clk.tick(fps)
